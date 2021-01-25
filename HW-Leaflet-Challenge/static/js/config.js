22dae354658b110cd148f0d2dcf8fd887000b886
John H Klinges <70554411+jklinges@users.noreply.github.com>
2021-01-24 20:29:37 -0500

    Add files via upload

---
+++ HW-Leaflet-Challenge/static/js/config.js
@@ -0,0 +1,2 @@
+// API key
+const API_KEY = "pk.eyJ1IjoidmlzZWxpbWFqIiwiYSI6ImNrNWhlOGhtaDAyd24za3FyM3ltdjdwem4ifQ.e7v6oJX2zgBgq0tF6bsiog";
---
+++ HW-Leaflet-Challenge/static/js/config2.js
@@ -0,0 +1,3 @@
+// API key
+mapboxgl.accessToken = "pk.eyJ1IjoidmlzZWxpbWFqIiwiYSI6ImNrNWhlOGhtaDAyd24za3FyM3ltdjdwem4ifQ.e7v6oJX2zgBgq0tF6bsiog";
+
