ad8cbfa3605c91f36dbeee6d7da707aa8ed5a6d9
John H Klinges <70554411+jklinges@users.noreply.github.com>
2021-01-25 20:14:33 -0500

    Add files via upload

--- HW-Leaflet-Challenge/static/js/earthquake.js
+++ HW-Leaflet-Challenge/static/js/earthquake.js
@@ -1,18 +1,24 @@
-// Add base maps and change heat map colors
-  
+// Data Sources:
+
 var geoJsonLayer;
-// var queryUrl = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson"; 
 var queryUrl = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_month.geojson";
 
-var tectonicPlatesUrl = "https://raw.githubusercontent.com/fraxen/tectonicplates/master/GeoJSON/PB2002_boundaries.json"
+var tectonicPlatesUrl = "https://raw.githubusercontent.com/fraxen/tectonicplates/master/GeoJSON/PB2002_boundaries.json
 
-// Initialize  & Create all of the LayerGroups we'll be using:
+// Initialize LayerGroups and Define Varibles in Mapbox:
+// Choices include dark, light, satellite, topography, oceanbase, worldimaginery
+
 var WorldEarthquakes = new L.LayerGroup();
 var TectoncPlates = new L.LayerGroup();
 var heatLayer = new L.LayerGroup();
 
+var darkmap = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+    attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
+    maxZoom: 18,
+    id: "dark-v10",
+    accessToken: API_KEY
+});
 
-// Define Variables for Tile Layers
 var lightMap = L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
     attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
     tileSize: 512,
@@ -22,11 +28,11 @@ var lightMap = L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/
     accessToken: API_KEY
 });
 
-var lightGray = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
-    attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
-    maxZoom: 18,
-    id: "light-v10",
-    accessToken: API_KEY
+ var lightGray = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+        attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
+        maxZoom: 18,
+        id: "light-v10",
+        accessToken: API_KEY    
 });
 
 var satellite = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
@@ -41,20 +47,13 @@ var OpenTopoMap = L.tileLayer('https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png'
     attribution: 'Map data: &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, <a href="http://viewfinderpanoramas.org">SRTM</a> | Map style: &copy; <a href="https://opentopomap.org">OpenTopoMap</a> (<a href="https://creativecommons.org/licenses/by-sa/3.0/">CC-BY-SA</a>)'
 });
 
-var Esri_WorldImagery = L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}', {
-    attribution: 'Tiles &copy; Esri &mdash; Source: Esri, i-cubed, USDA, USGS, AEX, GeoEye, Getmapping, Aerogrid, IGN, IGP, UPR-EGP, and the GIS User Community'
-});
-
 var Esri_OceanBasemap = L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/Ocean_Basemap/MapServer/tile/{z}/{y}/{x}', {
     attribution: 'Tiles &copy; Esri &mdash; Sources: GEBCO, NOAA, CHS, OSU, UNH, CSUMB, National Geographic, DeLorme, NAVTEQ, and Esri',
-    maxZoom: 13
+    maxZoom: 17
 });
 
-var darkmap = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
-    attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
-    maxZoom: 18,
-    id: "dark-v10",
-    accessToken: API_KEY
+var Esri_WorldImagery = L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}', {
+    attribution: 'Tiles &copy; Esri &mdash; Source: Esri, i-cubed, USDA, USGS, AEX, GeoEye, Getmapping, Aerogrid, IGN, IGP, UPR-EGP, and the GIS User Community'
 });
 
 var Stamen_TonerBackground = L.tileLayer('https://stamen-tiles-{s}.a.ssl.fastly.net/toner-background/{z}/{x}/{y}{r}.{ext}', {
@@ -81,40 +80,35 @@ var Stamen_TonerLite = L.tileLayer('https://stamen-tiles-{s}.a.ssl.fastly.net/to
 	ext: 'png'
 });
 
-
-// Define baseMaps Object to Hold Base Layers
 var baseMaps = {
+    "Dark Map": darkmap,
     "Light Gray": lightGray,
     "Light Map": lightMap,
-    "Dark Map": darkmap,
     "Satellite Map": satellite,
     "Topography Map": OpenTopoMap,
-    "Esri World Imagery": Esri_WorldImagery,
     "Esri Ocean Base": Esri_OceanBasemap,
+    "Esri World Imagery": Esri_WorldImagery,
     "Toner-Sea w/ Names" : Stamen_Toner,
     "Toner Background" : Stamen_TonerBackground,
     "Grey-sea w/ Names" : Stamen_TonerLite
 };
 
-// Create an overlays object to add to the layer control
+// Create an overlays, map layers and overlay layers
 var overlays = {
     "Earth Quake Magnitude": WorldEarthquakes,
     "Tectonic Plates": TectoncPlates,
     "Heat Map": heatLayer,
-   
 };
 
-//Create Map and Default layers to display on Load
 var myMap = L.map("map", {
     center: [37.09, -95.71],
     zoom: 5,
     layers: [lightMap, WorldEarthquakes]
 });
 
-// Create a control for our layers, add our overlay layers to it
 L.control.layers(baseMaps, overlays).addTo(myMap);
 
-
+// Parse dealer location 
 
 d3.json(queryUrl, function (data) {
 
@@ -169,8 +163,8 @@ d3.json(queryUrl, function (data) {
 
     L.heatLayer(heatArray, {
         minOpacity: 0.2,
-        radius: 25,
-        blur: 15,
+        radius: 24,
+        blur: 14,
         gradient: {0.9:'Red', 0.8: '#ff5f00', 0.7: 'Orange', 0.6: 'Yellow', 0.5: 'lime', 0.4: 'Aqua', 0.3: 'DarkBlue', 0.2: 'DarkBlue', 0.1: 'Purple'}
     }).addTo(heatLayer);
     heatLayer.addTo(myMap)
@@ -209,37 +203,9 @@ d3.json(queryUrl, function (data) {
         div.innerHTML = labels.join('<br>');
         return div;
 
-
-
     };
 
-    // Adding legend to the map
+    // Create map legend
     legend.addTo(myMap);
 
-
 });
-
-/****MY Original Colors**************************************** */
-// data.features.forEach((element) => {
-//     color = "red";
-//     // Conditionals for countries points
-//     if (element.properties.mag > 6) {
-//         color = "#5f0000";
-//     } else if (element.properties.mag > 5) {
-//         color = "Maroon";
-//     } else if (element.properties.mag > 4) {
-//         color = "purple";
-//     } else if (element.properties.mag > 3) {
-//         color = "MediumVioletRed";
-//     } else if (element.properties.mag > 2) {
-//         color = "Fuchsia";
-//     } else if (element.properties.mag > 1) {
-//         color = "Teal";
-//     } else if (element.properties.mag > 0) {
-//         color = "green";
-//     } else if (element.properties.mag > -1) {
-//         color = "Olive";
-//     }
-
-// colors = ["#5f0000", "#800000", "#800080", "#af0087", "#ff00ff", "#008080", "#008000", "#808000"];
-
